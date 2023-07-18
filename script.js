8b7b4307e16e7b385277f36335c6c1e1e7000a18
vahanafk <vahan.ghazaryan.00@mail.ru>
2023-07-18 21:06:51 +0400

    elements received in js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-let player1 = document.getElementsByClassName('player1');
-let player2 = document.getElementsByClassName('player2');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const playBtn = document.getElementById('play_button');
+const board = document.getElementById("board");
+const ball = document.getElementById("ball");
+ball.direction = 
+
+playBtn.addEventListener("click", startGame, true);
+
+function startGame() {
+    const playBtnDiv = document.getElementById("id_play_button_div");
+    console.log(playBtnDiv);
+    playBtnDiv.style.display = "none";
+    startBall();
+}
+
+function startBall() {
+    let pos = ball.getBoundingClientRect();
+    let id = null;
+    // clearInterval()
+    // const myInterval = setInterval(ballMove);
+    console.log(ball);
+    console.log(pos);
+    function ballMove() {
+    }
+}
